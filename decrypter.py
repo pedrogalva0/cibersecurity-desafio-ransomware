950fc44868f2a924f19333f9d7ea3a57fa1dfd01
pedrogalva0 <167096454+pedrogalva0@users.noreply.github.com>
2025-03-23 02:17:05 -0300

    decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,28 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def decrypt_file(encrypted_file_name, key, original_file_name):
+    try:
+        
+        with open(encrypted_file_name, "rb") as file:
+            file_data = file.read()
 
-## chave para descriptografia
+        aes = pyaes.AESModeOfOperationCTR(key)
+        decrypt_data = aes.decrypt(file_data)
+
+        with open(original_file_name, "wb") as new_file:
+            new_file.write(decrypt_data)
+
+        os.remove(encrypted_file_name)
+        print(f"Arquivo '{encrypted_file_name}' descriptografado com sucesso e salvo como '{original_file_name}'.")
+
+    except Exception as e:
+        print(f"Erro ao descriptografar o arquivo: {e}")
+
 key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
-os.remove(file_name)
+encrypted_file_name = "teste.txt.ransomwaretroll"
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+original_file_name = "teste.txt"
+
+decrypt_file(encrypted_file_name, key, original_file_name)
